7d28d53e2aa2b65e7bd1861ea13e59cdd5b146df
occume <occume@gmail.com>
2014-07-25 18:05:53 +0800

    增加ignore

---
+++ src/test/java/org/d3/test/redis/RedisExample.java
@@ -0,0 +1,38 @@
+package org.d3.test.redis;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.annotation.Resource;
+
+import org.d3.D3SpringConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisExample {
+	
+//	 @Autowired
+//	 private RedisTemplate<String, String> template;
+
+	    // inject the template as ListOperations
+	    // can also inject as Value, Set, ZSet, and HashOperations
+//	 @Resource(name="redisTemplate")
+//	 private ListOperations<String, String> listOps;
+
+	 public void addLink(String userId, URL url) {
+//		 listOps.leftPush(userId, url.toExternalForm());
+	     // or use template directly
+//		 template.boundListOps(userId).leftPush(url.toExternalForm());
+	 }
+
+	public static void main(String[] args) throws MalformedURLException {
+		AbstractApplicationContext context = new AnnotationConfigApplicationContext(D3SpringConfig.class);
+		
+//		RedisExample e = (RedisExample) context.getBean("redisExample");
+//		e.addLink("occume", new URL("http://www.t7joy.com"));
+	}
+
+}
